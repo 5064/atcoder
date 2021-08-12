3d6ce9552e8c3d6726c5a4741e16cb69ae96bf00
5064 <5og4.kirin@gmail.com>
2021-08-12 22:10:12 +0900

    update

--- ABC/129/D.py
+++
@@ -1,7 +0,0 @@
-H, W = map(int, input().split())
-S = [int(input()) for _ in range(M)]
-
-L = [[0]*H for _ in range(W)]
-R = [[0]*H for _ in range(W)]
-U = [[0]*H for _ in range(W)]
-D = [[0]*H for _ in range(W)]
