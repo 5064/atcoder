f44a7bc1199539ad547d391b483115ab9cab32f6
5064 <5og4.kirin@gmail.com>
2021-10-17 14:14:16 +0900

    update

---
+++ ABC/086/B.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n) for (ll i = 0; i < (n); ++i)
+#define rep2(i, n) for (ll i = 0; i < (n); ++i)
+#define rep3(i, a, b) for (ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for (ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rrep1(n) for (ll i = (n); i--;)
+#define rrep2(i, n) for (ll i = (n); i--;)
+#define rrep3(i, a, b) for (ll i = (b); i-- > (a);)
+#define rrep4(i, a, b, c) for (ll i = (a) + ((b) - (a)-1) / (c) * (c); i >= (a); i -= c)
+#define rrep(...) overload4(__VA_ARGS__, rrep4, rrep3, rrep2, rrep1)(__VA_ARGS__)
+//x:コンテナ
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b; // aをbで更新
+        return true;
+    }
+    return false;
+}
+
+//---------------------------------------
+
+int main()
+{
+    string a = "", b = "";
+    cin >> a >> b;
+
+    ll ab = stoi(a + b);
+    ll i = 1;
+    ll sn = 0;
+    string ans = "No";
+    while (ab > sn)
+    {
+        sn = i * i;
+        if (sn == ab)
+        {
+            ans = "Yes";
+            break;
+        }
+        i++;
+    }
+    cout << ans << endl;
+}
